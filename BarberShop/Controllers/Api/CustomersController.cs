891f4cc4223d5d21689dbc335aeb84b06788ce72
Ahmed Salad <Ahmed.Salad@brightstar.com>
2019-11-19 13:40:56 +0000

    Swapped footer for a nav bar and fixed it at the bottom.

--- BarberShop/Controllers/Api/CustomersController.cs
+++ BarberShop/Controllers/Api/CustomersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using BarberShop.Controllers.Dto;
 using AutoMapper;
+using System.Web;
 
 
 namespace BarberShop.Models
@@ -16,102 +17,102 @@ namespace BarberShop.Models
     public class CustomersController : ControllerBase
     {
 
-        private readonly BarberShopDbContext _context;
-        private readonly IMapper _mapper;
-        public CustomersController(IMapper mapper, BarberShopDbContext context)
-        {
-            _mapper = mapper;
-            _context = context;
-        }
+       // private readonly BarberShopDbContext _context;
+       // private readonly IMapper _mapper;
+       // public CustomersController(IMapper mapper, BarberShopDbContext context)
+       // {
+       //     _mapper = mapper;
+       //     _context = context;
+       // }
 
-        [HttpGet]
-        public IEnumerable<CustomerDto> GetCustomers()
-        {
-
-
-            return _context.Customers.ToList().Select(_mapper.Map<Customer,CustomerDto>);
-       }
-
-        [HttpGet]
-        [Route("GetCustomers")]
-        public CustomerDto GetCustomer(int id)
-        {
-            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
-
-            if(customerInDB == null)
-            {
-                throw new System.Web.Http.HttpResponseException(HttpStatusCode.BadRequest);
-            }
-
-          
-
-            return _mapper.Map<Customer, CustomerDto>(customerInDB); 
-        }
+       // [HttpGet]
+       // public IEnumerable<CustomerDto> GetCustomers()
+       // {
 
 
-        [HttpPost]
-        [Route("CreateCustomer")]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
-        {
-           
-            if (!ModelState.IsValid)
-            {
-                throw new System.Web.Http.HttpResponseException(HttpStatusCode.BadRequest);
-            }
+       //     return _context.Customers.ToList().Select(_mapper.Map<Customer,CustomerDto>);
+       //}
 
-            var customer = _mapper.Map<CustomerDto, Customer>(customerDto);
-            _context.Customers.Add(customer);
-            _context.SaveChanges();
+       // [HttpGet]
+       // [Route("GetCustomers")]
+       // public CustomerDto GetCustomer(int id)
+       // {
+       //     var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-            customerDto.Id = customer.Id;
-
-            return customerDto;
-        }
-
-
-
-        [HttpPut]
-        [Route("UpdateCustomer")]
-        public CustomerDto UpdateCustomer(int Id, CustomerDto customerDto)
-        {
+       //     if(customerInDB == null)
+       //     {
+       //         throw new System.Web.Http.HttpResponseException(HttpStatusCode.BadRequest);
+       //     }
 
           
 
-            if (!ModelState.IsValid)
-            {
-                throw new System.Web.Http.HttpResponseException(HttpStatusCode.BadRequest);
-            }
+       //     return _mapper.Map<Customer, CustomerDto>(customerInDB); 
+       // }
 
-            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == Id);
 
-            if (customerInDB == null)
-            {
-                throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            var customer = _mapper.Map<CustomerDto, Customer>(customerDto, customerInDB);
-            _context.Customers.Update(customer);
-            _context.SaveChanges();
-
-            return customerDto;
-        }
-
-        [HttpDelete]
-        [Route("DeleteCustomer")]
-        public void DeleteCustomer(int Id)
-        {
-
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
-
-            if (customer == null)
-            {
-                throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            _context.Customers.Remove(customer);
-            _context.SaveChanges();
+       // [HttpPost]
+       // [Route("CreateCustomer")]
+       // public CustomerDto CreateCustomer(CustomerDto customerDto)
+       // {
            
-        }
+       //     if (!ModelState.IsValid)
+       //     {
+       //         throw new System.Web.Http.HttpResponseException(HttpStatusCode.BadRequest);
+       //     }
+
+       //     var customer = _mapper.Map<CustomerDto, Customer>(customerDto);
+       //     _context.Customers.Add(customer);
+       //     _context.SaveChanges();
+
+       //     customerDto.Id = customer.Id;
+
+       //     return customerDto;
+       // }
+
+
+
+       // [HttpPut]
+       // [Route("UpdateCustomer")]
+       // public CustomerDto UpdateCustomer(int Id, CustomerDto customerDto)
+       // {
+
+          
+
+       //     if (!ModelState.IsValid)
+       //     {
+       //         throw new System.Web.Http.HttpResponseException(HttpStatusCode.BadRequest);
+       //     }
+
+       //     var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == Id);
+
+       //     if (customerInDB == null)
+       //     {
+       //         throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
+       //     }
+
+       //     var customer = _mapper.Map<CustomerDto, Customer>(customerDto, customerInDB);
+       //     _context.Customers.Update(customer);
+       //     _context.SaveChanges();
+
+       //     return customerDto;
+       // }
+
+       // [HttpDelete]
+       // [Route("DeleteCustomer")]
+       // public void DeleteCustomer(int Id)
+       // {
+
+       //     var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+
+       //     if (customer == null)
+       //     {
+       //         throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
+       //     }
+
+       //     _context.Customers.Remove(customer);
+       //     _context.SaveChanges();
+           
+       // }
 
 
     }

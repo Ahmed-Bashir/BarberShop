611cb2893ed93eff6be0a566764b85178ff88202
Ahmed Bashir <tull.sull@hotmail.com>
2020-01-17 11:50:45 +0000

    commit

---
+++ BarberShop/Migrations/20191129122531_populateSeedCutsTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BarberShop.Migrations
+{
+    public partial class populateSeedCutsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cuts",
+                columns: new[] { "Id", "Cuts", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Regular Cut", 15 },
+                    { 2, "Childrens Cut", 6 },
+                    { 3, "Beard Trim", 5 },
+                    { 4, "Mustache Trim", 3 },
+                    { 5, "Shape Ups", 3 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cuts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cuts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cuts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cuts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cuts",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
---
+++ BarberShop/Migrations/20191129152409_seedBarberTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BarberShop.Migrations
+{
+    public partial class seedBarberTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Barbers",
+                columns: new[] { "Id", "Name", "Number", "Shop" },
+                values: new object[] { 1, "Kabrone", 78, "Brixton" });
+
+            migrationBuilder.InsertData(
+                table: "Barbers",
+                columns: new[] { "Id", "Name", "Number", "Shop" },
+                values: new object[] { 2, "Andre", 208, "Stockwell" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Barbers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Barbers",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
